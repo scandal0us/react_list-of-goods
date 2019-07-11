7751bf4794000710002af8344c29f562b9a5f1c9
scandal0us <d.l.lutsak@gmail.com>
2019-07-11 17:44:06 +0300

    Updates

---
+++ precache-manifest.2983a2fe73a41fb8114467b5b12c0802.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3f66a05211a42b2d53dc5b39742bd1",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_list-of-goods/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9fdbd08dcd633b49858e",
+    "url": "/react_list-of-goods/static/js/main.2915c9ce.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.2983a2fe73a41fb8114467b5b12c0802.js"
 );
 
 self.addEventListener('message', (event) => {
